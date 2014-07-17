ffc8824b6631de08115034c3212644e56a8b672c
Alex Scherb <awscherb@gmail.com>
2014-07-17 17:15:00 -0400

    Updated ScheduleLocation

--- src/com/awscherb/foodtrucker/ScheduleLocation.java
+++ src/com/awscherb/foodtrucker/ScheduleLocation.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 
 public class ScheduleLocation {
     
+    /** Time of deal, i.e. "10AM - 3PM"  */
     private String time;
+    /** Meal of the day */
+    private String meal;
+    /** Location of the truck */
     private String location;
+    /** Collection of trucks at that location */
     private Collection<FoodTruck> trucks;
 
 }
